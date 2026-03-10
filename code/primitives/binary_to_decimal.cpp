410dc9cdf06faa6ae17c14ac89dae82ece81c6e5
Élvis Corrêa Miranda Júnior <elviscorreamirandajr@gmail.com>
2026-03-10 09:56:49 -0300

    update

--- code/primitives/binary_to_decimal.cpp
+++ code/primitives/binary_to_decimal.cpp
@@ -2,7 +2,7 @@
 
 int binaryToDecimal(const string& binary) {
     int decimal = 0;
-    for (size_t i = 0; i < binary.length(); ++i) {
+    for (size_t i = 0; i < binary.length(); i++) {
         if (binary[binary.length() - 1 - i] == '1') {
             decimal += (1 << i); // Add 2^i for each '1' found
         }
