dba11204c948196ebacaf4f21594d9046da96dfa
Élvis Corrêa Miranda Júnior <elviscorreamirandajr@gmail.com>
2026-04-10 11:21:49 -0300

    Revert "update powmod"
    This reverts commit d3da6f40d4d3064e7eff7fd73d96d2ba26ae39d5.

--- code/math/modular_exponentiation.cpp
+++ code/math/modular_exponentiation.cpp
@@ -5,13 +5,15 @@
 int powMod(int x, int n, int MOD){
     int res = 1;
 
-    while(n > 0){
-        if(n&1){
+    while(n >= 1){
+        if(n & 1){
             res = (res * x) % MOD;
+            n--;
         }
         else{
             x = (x * x) % MOD;
-        } n >>= 1;
+            n /= 2;
+        }
     }
     return res;
 }
