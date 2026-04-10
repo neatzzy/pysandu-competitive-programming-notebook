d3da6f40d4d3064e7eff7fd73d96d2ba26ae39d5
Élvis Corrêa Miranda Júnior <elviscorreamirandajr@gmail.com>
2026-04-10 11:06:29 -0300

    update powmod

--- code/math/modular_exponentiation.cpp
+++ code/math/modular_exponentiation.cpp
@@ -5,15 +5,13 @@
 int powMod(int x, int n, int MOD){
     int res = 1;
 
-    while(n >= 1){
-        if(n & 1){
+    while(n > 0){
+        if(n&1){
             res = (res * x) % MOD;
-            n--;
         }
         else{
             x = (x * x) % MOD;
-            n /= 2;
-        }
+        } n >>= 1;
     }
     return res;
 }
