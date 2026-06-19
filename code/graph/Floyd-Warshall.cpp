5c4986efb2d761e7d9e5367fcf16cb9ed4f79ee7
Élvis Corrêa Miranda Júnior <elviscorreamirandajr@gmail.com>
2026-06-19 09:18:44 -0300

    update

--- code/graph/Floyd-Warshall.cpp
+++ code/graph/Floyd-Warshall.cpp
@@ -2,25 +2,17 @@
 // Time Complexity: O(V^3)
 // Space Complexity: O(V^2)
 
-void floydWarshall(Graph& graph, vector<vector<int>>& dist) {
-    int V = graph.size();
-    
-    for(int i = 0; i < V; i++){
-        for (int j = 0; j < V; j++) {
-            if (i == j) {
-                dist[i][j] = 0;
-            } else if (dist[i][j] == 0) {
-                dist[i][j] = INT_MAX; // No edge
-            }
-        }
-    }
+void floydWarshall(vector<vector<int>> &dist) {
+    int V = dist.size();
+    int INF = 1e8;
 
     for (int k = 0; k < V; k++) {
+
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
-                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
+                if(dist[i][k] != INF && dist[k][j]!= INF )
+                    dist[i][j] = min(dist[i][j],
+                                     dist[i][k] + dist[k][j]);
             }
         }
     }
